edc4fa0d178a50803f2887790728f56c6fa15aeb
richiesure <44743786+richiesure@users.noreply.github.com>
2025-08-01 00:49:29 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,11 @@
+variable "region" {
+  description = "AWS region for deployment"
+  type        = string
+  default     = "eu-west-2"
+}
+
 variable "ssh_key_name" {
-  description = "The name of the existing EC2 key pair (without .pem) used for SSH access to EKS worker nodes"
+  description = "The name of the EC2 key pair used for SSH access (without .pem)"
   type        = string
   default     = "tier-1"
 }
